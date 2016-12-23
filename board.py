c30a41a582c0a5aa5de357f1aa10440d7c74076c
Alex Beals <alexcbeals@gmail.com>
2016-12-23 13:38:16 -0800

    reorganized board/game/player structure, depth search for best move: 100k+ best high score with this method in ~50 games

--- board.py
+++ board.py
@@ -1,12 +1,161 @@
 # coding: utf-8
 
-import sys
+import sys, math
+
+class Move:
+  piece = None
+  loc = None
+
+  def __init__(self, piece, loc):
+    self.piece = piece
+    self.loc = loc
 
 class Board:
   board = []
+  score = 0
 
-  def __init__(self, size):
-    self.create_board(size)
+  def __init__(self, moves):
+    self.board = []
+    self.score = 0
+
+    self.create_board(9)
+
+    # Iterate through all of the previous moves
+    for move in moves:
+      self.make_move(move)
+
+  def duplicate(self):
+    b = Board([])
+    b.board = [x[:] for x in self.board]
+    b.score = self.score
+    return b
+
+  # Get all valid moves
+  def get_moves(self, pieces):
+    valid = []
+
+    for piece in pieces:
+      for row in range(len(self.board)):
+        for pos in range(len(self.board[row])):
+          if self.can_place_piece(piece, [row, pos]):
+            valid.append(Move(piece, [row, pos]))
+
+    return valid
+
+  # Make a move
+  def make_move(self, move):
+    piece = move.piece
+    loc = move.loc
+
+    # Place the piece
+    self.place_piece(piece, loc)
+
+    # Check to see if any rows were completed (by the piece)
+    rows = self.completed_rows(piece, loc)
+
+    # Handles scoring
+    score = 40 # for placing a piece
+    if len(rows) > 0:
+      base = 10 * (len(rows) + 1)
+      for i, row in enumerate(rows):
+        # Number of pieces * (base score * 1.2^row index)
+        score += int(row[2] * (math.floor((base * max(1.2**i, 1)))))
+
+    # Clears those rows
+    for row in rows:
+      self.clear_row(row)
+
+    self.score += score
+
+  # Checks to see if there are any full rows
+  # Rows are defined by a starting position, a direction, and a length (for calculation ease)
+  def completed_rows(self, piece, loc):
+    piece_locations = [loc]
+    for d in piece.path:
+      loc = self.next_loc(loc, d)
+      piece_locations.append(loc)
+
+    completed = []
+
+    for location in piece_locations:
+      # Check each 'row' the piece is in to see if any of them are full
+      # Horizontal
+      loc = location[:]
+      found_horizontal = True
+      for pos in range(0, len(self.board[loc[0]])):
+        if self.val([loc[0], pos]) == 0:
+          found_horizontal = False
+          break
+      if found_horizontal:
+        row = [[location[0], 0], 'r', len(self.board[location[0]])]
+        if row not in completed:
+          completed.append(row)
+
+      # Down-right
+      loc = location[:]
+      found_dr = True
+      top = []
+      c = 0
+      try:
+        while (loc[0] >= 0 and loc[1] >= 0):
+          if self.val(loc) == 0:
+            found_dr = False
+            break
+          top = loc[:]
+          loc = self.next_loc(loc, 'ul')
+          c+=1
+      except Exception as e:
+        pass
+
+      loc = location[:]
+      c-=1 # don't double count point
+      try:
+        while (loc[0] >= 0 and loc[1] >= 0):
+          if self.val(loc) == 0:
+            found_dr = False
+            break
+          loc = self.next_loc(loc, 'dr')
+          c+=1
+      except Exception as e:
+        pass
+      if found_dr:
+        row = [top, 'dr', c]
+        if row not in completed:
+          completed.append(row)
+
+      # Down-left
+      loc = location[:]
+      found_dl = True
+      top = []
+      c = 0
+      try:
+        while (loc[0] >= 0 and loc[1] >= 0):
+          if self.val(loc) == 0:
+            found_dl = False
+            break
+          top = loc[:]
+          loc = self.next_loc(loc, 'ur')
+          c+=1
+      except Exception as e:
+        pass
+
+      loc = location[:]
+      c-=1 # don't double count point
+      try:
+        while (loc[0] >= 0 and loc[1] >= 0):
+          if self.val(loc) == 0:
+            found_dl = False
+            break
+          loc = self.next_loc(loc, 'dl')
+          c+=1
+      except Exception as e:
+        pass
+      if found_dl:
+        row = [top, 'dl', c]
+        if row not in completed:
+          completed.append(row)
+
+    return completed
 
   # Places the piece on the board
   def place_piece(self, piece, loc):
--- hex.py
+++ hex.py
@@ -1,93 +1,69 @@
 from timing import timing
-from board import Board
-from game import Game
-import math, pickle
+from board import Board, Move
+from pieces import new_piece
+import math, time
 
-from neat import nn, population, config
+def copy_board(board):
+  return board.duplicate()
 
 class Player:
-  def __init__(self, genome):
-    self.genome = genome
-    self.nn = nn.create_feed_forward_phenotype(genome)
-    self.game = Game()
+  def __init__(self):
+    self.pieces = [new_piece(), new_piece(), new_piece()]
+    self.moves = []
     self.done = False
 
-    # Generate quick way to find the location
-    self.locs = []
-    for row in range(len(self.game.board.board)):
-      for pos in range(len(self.game.board.board[row])):
-        self.locs.append([row, pos])
+  def append_move(self, board, move):
+    b = copy_board(board)
+    b.make_move(move)
+    return b
 
-  def make_move(self):
-    # Input 1: the board in an array of 0's and 1's
-    board = [1 if x > 0 else 0 for x in sum(self.game.board.board, [])]
+  def weight(self, b, move, pieces, depth, max_depth):
+    if (depth == max_depth):
+      return 0
 
-    #Input 2: the pieces available in an array of 3 numbers representing the pieces
-    pieces = [x.index for x in self.game.pieces]
+    board = self.append_move(b, move)
+    w = board.score
+    for move in board.get_moves(pieces):
+      p = pieces[:]
+      p.remove(move.piece)
 
-    outputs = self.nn.serial_activate(board + pieces)
+      w += self.weight(board, move, p, depth + 1, max_depth)
+    return w
 
-    loc = self.locs[int((outputs[0] - 1e-16) * (len(self.locs)))]
-    piece = self.game.pieces.pop(int((outputs[1] - 1e-16) * 3))
+  def get_best_move(self, depth):
+    board = Board(self.moves)
+    valid_moves = board.get_moves(self.pieces)
+    if (len(valid_moves) < 50):
+      depth += 1
 
-    self.game.add_piece()
+    best_weight = 0
+    best_move = None
+    for move in valid_moves:
+      w = self.weight(board, move, self.pieces, 0, depth)
+      if w > best_weight:
+        best_weight = w
+        best_move = move
 
-    # Check if it's a valid move
-    if (self.game.board.can_place_piece(piece, loc)):
-      self.game.make_move(piece, loc)
-      return True
+    return best_move
+
+  def play(self):
+    move = self.get_best_move(2)
+
+    if (move == None):
+      self.done = True
     else:
-      return False
+      self.moves.append(move)
+      self.pieces.remove(move.piece)
+      self.pieces.append(new_piece())
 
-generations = 0
-best_fitness = 0
-fitness_history = []
-best_genome = None
+  def status(self):
+    board = Board(self.moves)
+    print board.score
+    board.print_self()
 
-# Run one set of evolutions
-def evolve(genomes):
-  global best_fitness, best_genome, generations
+player = Player()
+while (not player.done):
+  player.play()
+  player.status()
 
-  gen_best = 0
-
-  generations += 1
-
-  print " ******** Generation " + str(generations) + " ********"
-
-  players = []
-  for genome in genomes:
-    players.append(Player(genome))
-
-  # Keep playing until every game is finished
-  still_playing = len(players)
-  while (still_playing):
-    for player in players:
-      if player.done:
-        continue
-      else:
-        success = player.make_move()
-        if (not success or player.game.game_over()):
-          player.done = True
-          still_playing -= 1
-          player.genome.fitness = player.game.score
-
-          # Save the generational best
-          if (player.game.score > gen_best):
-            gen_best = player.game.score
-
-          # Save the current best genome
-          if (player.game.score > best_fitness):
-            best_fitness = player.game.score
-            best_genome = player.genome
-            fitness_history.append(best_fitness)
-            player.game.print_board()
-            with open('curr_best_genome_2', 'wb') as f:
-              pickle.dump(best_genome, f)
-
-  print "Best player: " + str(gen_best)
-
-config = config.Config('hex_config')
-config.report = False
-pop = population.Population(config)
-pop.run(evolve, 2500)
-print fitness_history
+player.status()
